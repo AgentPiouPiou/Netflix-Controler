2c26eb24558e11dd10f164297d06679d74d23518
Mathys <mathyswl10@gmail.com>
2026-05-05 21:45:43 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+let action = null;
+
+const API_URL = "https://ton-api-render.onrender.com/action"; // à remplacer
+
+function triggerAction(type) {
+    action = type;
+    updateUI();
+
+    // Envoi à l'API
+    sendToAPI(type);
+
+    // Reset après 1 seconde
+    setTimeout(() => {
+        action = null;
+        updateUI();
+    }, 1000);
+}
+
+function updateUI() {
+    document.getElementById("status").innerText = "Action : " + action;
+}
+
+function sendToAPI(type) {
+    fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ action: type })
+    })
+    .then(res => res.json())
+    .then(data => console.log("Réponse API:", data))
+    .catch(err => console.error("Erreur API:", err));
+}
